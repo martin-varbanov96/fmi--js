9334125371180a3e933a51ed3e9f3d9a4fff56e0
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-11-01 23:24:56 +0200

    fib
    completed first 2 fibunacci tasks

---
+++ task1/phiEstimation/app.js
@@ -0,0 +1,15 @@
+﻿function phiEstimation(num) { 
+    if (num == 1) {
+        return 0;
+    }
+    var b = 1;
+    var start = 0;
+    for (var i = 1; i <= num; i++) {
+        start += b;
+        b = start - b;
+    }
+    return start/b;   
+}
+
+//Enter number
+console.log(phiEstimation(27));
---
+++ task1/task1/app.js
@@ -0,0 +1,16 @@
+﻿function getNthFibNumber(num){
+    if (num == 1) {
+        return 0;
+    }  
+    var b = 1;
+    var start = 0;
+    for (var i = 1; i <= num; i++) {                
+        start += b;
+        b = start - b;
+    }
+    return start;
+}
+
+
+//Enter number
+console.log(getNthFibNumber(3));
