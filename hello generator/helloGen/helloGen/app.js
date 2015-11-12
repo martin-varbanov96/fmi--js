6e14d05fca86946088608d0878ae9519f6268139
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-11-12 16:55:33 +0200

    generators
    done some generators

---
+++ hello generator/helloGen/helloGen/app.js
@@ -0,0 +1,9 @@
+﻿function* simpleGenerator() {
+    var current = 0;
+    while (current <=5) {         
+        yield current++;
+    }
+}
+
+var a = simpleGenerator();
+console.log(a.next());
---
+++ recurenceRelation/recurenceRelation/app.js
@@ -0,0 +1,32 @@
+﻿var n = 5;
+var k = 3;
+
+function rabbitSeq(num, kum) {
+    var birthable = 0;
+    var newBorn = 0;
+    var maturing = 0;
+    var step = 0;
+    var deathrattle = 0;
+
+    for (var i = 1; i <= num; i++) {
+        deathrattle++;
+        if (deathrattle == kum) {
+            birthable--;
+            deathrattle = 0;
+        }
+        if (step == 0) { 
+            birthable = 0;
+            newBorn = 1;
+            maturing = 0;
+            step++;
+        } 
+        else {             
+            birthable += maturing;
+            maturing = newBorn;
+            newBorn = birthable * 1;
+        }
+    }
+    console.log(birthable + maturing + newBorn);
+}
+
+rabbitSeq(6, 3);
